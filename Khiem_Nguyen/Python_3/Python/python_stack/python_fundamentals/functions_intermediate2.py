486cf419e8dab102e10f36d8f6cbd25a537af532
Khiem Nguyen <kdnguyen11@gmail.com>
2018-09-07 06:56:26 -0500

    made changes to #8 and #9 for for_loop_basic2.py from feedback

---
+++ Khiem_Nguyen/Python_3/Python/python_stack/python_fundamentals/functions_intermediate2.py
@@ -0,0 +1,20 @@
+x = [ [5,2,3], [10,8,9] ]
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# #1a
+# z[0]["x"] = 15
+# print(z)
+
+
+
+# #1b
+# students[0]["last_name"]= "Bryant"
+# print(students)
