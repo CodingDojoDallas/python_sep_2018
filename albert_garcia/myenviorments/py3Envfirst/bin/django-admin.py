f0284467b79e1518fb168f29aa5bfd961e059f19
Albert Garcia <albertgarcia@Alberts-MacBook-Pro.local>
2018-09-07 16:19:04 -0500

    added slist

--- albert_garcia/myenviorments/py3Envfirst/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/albertgarcia/Desktop/Dojo/python_stack/myenviorments/py3Envfirst/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
